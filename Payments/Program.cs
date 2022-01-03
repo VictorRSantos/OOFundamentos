f0a22415c65b947f83b26ea547b096eda21fcb2a
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2022-01-03 20:54:14 -0300

    Primeiro commit

---
+++ Payments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Payments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Pagamento
+    {
+        // Propriedades
+        public DateTime Vencimento;
+
+        // Métodos
+        public void Pagar()
+        {
+
+
+        }
+
+
+        class PagamentoBoleto : Pagamento
+        {
+
+        }
+
+
+
+
+    }
+}
